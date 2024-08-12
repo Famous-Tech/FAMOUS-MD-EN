87c1a564686131c502a9de9c13541fbeb601e311
FD LORD / FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-08-12 11:57:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require("fs").promises;
 const path = require("path");
 const config = require("./config");
 const connect = require("./lib/connection");
-const { loadSession } = require("baileys");
 const io = require("socket.io-client");
 const { getandRequirePlugins } = require("./assets/database/plugins");
+const readline = require('readline');
+const { default: makeWASocket, generatePairingCode } = require('@whiskeysockets/baileys');
 
 global.__basedir = __dirname; // Set the base directory for the project
 
@@ -22,18 +23,32 @@ const readAndRequireFiles = async (directory) => {
   }
 };
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
+
 async function initialize() {
   console.log("Starting FAMOUS-MD");
   try {
-    if (config.SESSION_ID && !(await fs.stat("session").catch(() => false))) {
-      console.log("loading session from session id...");
-      await fs.mkdir("./session");
-      const credsData = await loadSession(config.SESSION_ID);
-      await fs.writeFile(
-        "./session/creds.json",
-        JSON.stringify(credsData.creds, null, 2)
-      );
+    // Demander le numéro de téléphone de l'utilisateur
+    let phoneNumber = await question('Please enter your WhatsApp number (with country code, e.g., +50943782508): ');
+    phoneNumber = phoneNumber.replace(/[^0-9]/g, '');
+
+    if (!phoneNumber.startsWith('+')) {
+        console.log('Invalid phone number format. Make sure it starts with the country code.');
+        process.exit(1);
     }
+
+    const sock = makeWASocket();
+    
+    // Générer un code de pairing
+    let code = await sock.requestPairingCode(phoneNumber);
+    code = code?.match(/.{1,4}/g)?.join("-") || code;
+    console.log(`Your Pairing Code: ${code}`);
+
     await readAndRequireFiles(path.join(__dirname, "/assets/database/"));
     console.log("Syncing Database");
 
@@ -46,6 +61,9 @@ async function initialize() {
     const ws = io("https://socket.xasena.me/", { reconnection: true });
     ws.on("connect", () => console.log("Connected to server"));
     ws.on("disconnect", () => console.log("Disconnected from server"));
+    
+    rl.close();
+
     return await connect();
   } catch (error) {
     console.error("Initialization error:", error);
